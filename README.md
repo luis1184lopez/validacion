26b10f96d39073ed3167cfdbd9ff4e3ec9b8a997
luis1184lopez <luis1184lopezvelasquez@gmail.com>
2020-08-30 01:15:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngvalidation-ea3h2u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngvalidation-ea3h2u)
---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "Angular Form Validation Tutorial";
+  angForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    console.log("AppComponent::constructor");
+    this.createForm();
+  }
+
+  createForm() {
+    console.log("AppComponent::createForm");
+    this.angForm = this.fb.group({
+      name: ["", [Validators.required, Validators.maxLength(5)]],
+      surname: ["", Validators.required]
+    });
+
+    this.angForm.controls["name"].valueChanges.subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  onSubmit() {
+    if (this.angForm.valid) {
+      console.log(this.angForm.value);
+    } else {
+      alert("ERROR!");
+    }
+  }
+} // FIN class
